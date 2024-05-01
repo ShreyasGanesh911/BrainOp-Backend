361bc3f4f9d7cb4d7e103f4262924c862bb81561
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-01 13:13:44 +0530

    Code cleanup

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { signup,getData } = require('../controllers/user.controller')
 const Auth = require('../middlewares/auth')
+const AsyncHandler = require('../utils/AsyncHandler')
 
 const userRouter = express.Router()
 
+// Routes
 userRouter.post('/signup',signup)
-userRouter.get("/test",Auth,getData)
+userRouter.get("/data",Auth,getData)
+
 module.exports = userRouter
