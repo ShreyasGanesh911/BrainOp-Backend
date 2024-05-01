89d518d6585ff14269af52b47fb67c5933562b75
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-01 12:26:04 +0530

    Added user router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const { signup,getData } = require('../controllers/user.controller')
+const Auth = require('../middlewares/auth')
 
 const userRouter = express.Router()
 
+userRouter.post('/signup',signup)
+userRouter.get("/test",Auth,getData)
 module.exports = userRouter
