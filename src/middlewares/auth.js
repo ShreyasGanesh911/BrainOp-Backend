d5fb6a5378a9bf589076e042c9c9c5b616b4b947
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-01 13:13:13 +0530

    code clean up

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,13 +6,11 @@ require('dotenv').config()
 
 const Auth = AsyncHandler(async(req,res,next)=>{
     const {AuthToken} = req.cookies
-    //console.log(AuthToken)
     jwt.verify(AuthToken,process.env.JWT_KEY,async(err,users)=>{
         if(err){
             res.clearCookie("AuthToken")
             return next(new ErrorHandler(401,"Invalid authToken, need to login again"))
         }
-        console.log(users.response._id)
         const response = await user.findOne({_id:users.response._id})
         if(!response)
             return next(new ErrorHandler(404,"User not found"))
