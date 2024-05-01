bd75f12f8b31e080c793c8e853b421f16d944c69
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-01 13:13:00 +0530

    Added user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,26 @@
+const  jwt  = require("jsonwebtoken");
+const user = require("../Model/user.model");
+const bcrypt = require('bcrypt')
+const AsyncHandler = require("../utils/AsyncHandler");
+const ErrorHandler = require("../utils/ErrorHandler");
+const product = require("../Model/product.model");
+require('dotenv').config()
+const signup = AsyncHandler(async(req,res,next)=>{
+    const {name,userName,email,password} = req.body
+    let response = await user.find({$or:[{userName},{email}]})
+    const salt = bcrypt.genSaltSync(12)
+    const key = bcrypt.hashSync(password,salt)
+     response = await user.create({name,userName,email,password:key})
+    console.log(response)
+    const value = jwt.sign({response},process.env.JWT_KEY,{expiresIn:'4h'})
+    res.cookie('AuthToken',value,{maxAge:1000 * 60 * 60*4})
+    res.status(201).json({success:true,message:"User created successfully"})
+})
+
+const getData = AsyncHandler(async(req,res,next)=>{
+    const {limit,skip} = req.query
+    const response = await product.find().limit(limit||20).skip(skip||0)
+    res.status(200).json({success:true,response,limit:limit||20,total:100,skip:skip||0})
+})
+
+module.exports = {signup,getData}
