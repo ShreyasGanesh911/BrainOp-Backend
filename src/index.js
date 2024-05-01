e797aa5579cfeff4346dbe68d9380f5d82d07054
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-01 12:25:29 +0530

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const connectDataBase = require("./DB");
 const app = require("./app");
 require('dotenv').config()
 const port =  process.env.PORT ||4000
@@ -6,6 +7,10 @@ const port =  process.env.PORT ||4000
 
 
 // port
-app.listen(port,()=>{
-    console.log("Listening to port ",port)
-})
+    connectDataBase().then(()=>{
+        app.listen(port,()=>{
+            console.log("Listening to port ",port)
+        })
+    }).catch((e)=>{
+        console.log("Error connecting to mongodb",e)
+    })
